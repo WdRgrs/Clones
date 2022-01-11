e493d795fa3b829cf01336fbc25067bb5100ef9c
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-01-11 02:35:52 -0600

    Embed background video - initially youtube

--- src/netflix/NetflixHomePg.js
+++ src/netflix/NetflixHomePg.js
@@ -4,8 +4,29 @@ import NetflixNav from './NetflixNav'
 function NetflixHomePg() {
     return (
         <main id='netflix-main-container'>
-            <NetflixNav />
+            <NetflixNav 
+            />
             Netflix Browsing Page!
+            {/* <video
+                autoPlay={true}
+                muted 
+                loop 
+                id="video"
+            >
+                <source
+            src="https://www.youtube.com/embed/5MC1dFvCmC8" />
+            </video>   */}
+            <iframe id='video' src="https://www.youtube.com/embed/5MC1dFvCmC8?autoplay=1&rel=1&controls=0&iv_load_policy=3&modestbranding=1&loop=1" title='cobra-kai-background'
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            />
+
+            <div id='next'>Next</div>
+            <div id='next'>
+            <div id='boxes'>Boxes</div>
+            <div id='boxes'>Boxes</div>
+            <div id='boxes'>Boxes</div>
+            <div id='boxes'>Boxes</div>
+            </div>
         </main>
     )
 }
