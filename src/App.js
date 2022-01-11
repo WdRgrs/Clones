8145e3c03526f49f6a8267aaa538ab08898e14c0
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-01-11 00:11:27 -0600

    Clean up console bugs, refactor code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Google from './google/Google'
 import Home from './Home'
 import Midwestern from './midwestern/Midwestern'
