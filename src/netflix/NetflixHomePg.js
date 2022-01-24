047a70eac9f3841614628c1e40906137b4154118
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-01-24 13:10:45 -0600

    Fixed video sourcing issues

--- src/netflix/NetflixHomePg.js
+++ src/netflix/NetflixHomePg.js
@@ -13,11 +13,14 @@ function NetflixHomePg() {
         <main id='netflix-main-container'>
             <NetflixNav 
             />
-
-            <iframe id='video' src="https://www.youtube.com/embed/5MC1dFvCmC8?autoplay=1&rel=1&controls=0&iv_load_policy=3&modestbranding=1&loop=1" title='cobra-kai-background'
+            {/* <iframe id='video' src="https://www.youtube.com/embed/5MC1dFvCmC8?autoplay=1&rel=1&controls=0&iv_load_policy=3&modestbranding=1&loop=1" title='cobra-kai-background'
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            /> */}
+            <iframe id='video' src="https://player.vimeo.com/video/655516741?h=a72147ab90&autoplay=1&loop=1&title=0&byline=0&portrait=0" title='cobra-kai-background'
+            // allow="autoplay; fullscreen; picture-in-picture" 
+            allowFullScreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             />
-
             <div id='blocker'>Blocker div</div>
 
 {/* Map over categories array - reuse component */}
