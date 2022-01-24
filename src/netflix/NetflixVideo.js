0b1fb25b438d11501c9e35f0262092137acd3cbb
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-01-24 15:51:39 -0600

    Import font, update main title card

--- src/netflix/NetflixVideo.js
+++ src/netflix/NetflixVideo.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import { BiVolumeFull, BiVolumeMute } from "react-icons/bi";
+import { BsHandThumbsUp, BsHandThumbsDown } from "react-icons/bs";
+
 
 function NetflixVideo() {
     const [vol, setVol] = useState(1);
@@ -7,7 +9,6 @@ function NetflixVideo() {
     function handleMute() {
         vol ? setVol(0) : setVol(1)
     }
-
     return (
         <div>
             {/* <iframe id='video' src="https://www.youtube.com/embed/5MC1dFvCmC8?autoplay=1&rel=1&controls=0&iv_load_policy=3&modestbranding=1&loop=1" title='cobra-kai-background'
@@ -20,8 +21,35 @@ function NetflixVideo() {
             />
             {/*blocker div - maybe change to kobra kai info */}
             <div id='blocker'>
-                <div>
-                    Cobra Kai season 4
+                <div id='netflix-main-video-title'>
+                    <h2 id='deadstock-title'><span>C</span>obra <span>K</span>ai</h2>
+                    <section id='netflix-title-info'>
+                        <div>New</div>
+                        <div>2020</div>
+                        <div>TV-14</div>
+                        <div>4 Seasons</div>
+                    </section>
+                    <p id='netflix-title-about'>a;lkjdf a;lkjsdf ;lkajsdf ;lkajd</p>
+                    <section id='netflix-title-buttons'>
+                        <button>Play</button>
+                        <button>+ My List</button>
+                        <div>Like</div>
+                        <div>Dislike</div>
+                    </section>
+                    <section id='netflix-title-details'>
+                        <div>
+                            <h3>Starring:</h3>
+                            <p>Ralph Macchio, William Zabka</p>
+                        </div>
+                        <div>
+                            <h3>Genres:</h3>
+                            <p>TV Dramas</p>
+                        </div>
+                        <div>
+                            <h3>This show is:</h3>
+                            <p>Heartfelt, Exciting</p>
+                        </div>
+                    </section>
                 </div>
                 <div onClick={()=> handleMute()}>
                 <div id='netflix-volume-button'>
