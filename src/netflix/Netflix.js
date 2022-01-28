350d1c94c5166d119242a5925a731e69ff114f91
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-01-27 22:28:24 -0600

    a

--- src/netflix/Netflix.js
+++ src/netflix/Netflix.js
@@ -14,50 +14,52 @@ function Netflix() {
     }
 
     return (
-        <div className='netflix-landing'>
+        <div>
             <NetflixNav />
-            <div className='netflix-landing-container'>
-                <h1 className='netflix-landing-header'>
-                    Who's watching?
-                </h1>
+            <div className='netflix-landing'>
+                <div className='netflix-landing-container'>
+                    <h1 className='netflix-landing-header'>
+                        Who's watching?
+                    </h1>
 
-                <div className='netflix-profile-container'>
-                    <Link to='/netflix/browse'>
-                        <div className='netflix-profile-card'>
-                            <img src={profile1} className='netflix-profile-img' alt='first profile'></img>
-                            <p>You</p>
-                        </div>
-                    </Link>
-                    
-                    <a href='https://wdrgrs.github.io/profile' target='_blank' rel='noreferrer'>
-                        <div className='netflix-profile-card'>
-                            <img src={profile2} className='netflix-profile-img' alt='first profile'></img>
-                            <p>Wade</p>
-                        </div>
-                    </a>
+                    <div className='netflix-profile-container'>
+                        <Link to='/netflix/browse'>
+                            <div className='netflix-profile-card'>
+                                <img src={profile1} className='netflix-profile-img' alt='first profile'></img>
+                                <p>You</p>
+                            </div>
+                        </Link>
+                        
+                        <a href='https://wdrgrs.github.io/profile' target='_blank' rel='noreferrer'>
+                            <div className='netflix-profile-card'>
+                                <img src={profile2} className='netflix-profile-img' alt='first profile'></img>
+                                <p>Wade</p>
+                            </div>
+                        </a>
 
-                    <div className='netflix-profile-card' onClick={badPathClick}>
-                        <img src={profile3} className='netflix-profile-img' alt='first profile'></img>
-                        <p>Kids</p>
-                    </div>
-
-                    <div className='netflix-profile-card' onClick={badPathClick}>
-                        <div className='netflix-profile-img'>
-                            <FaPlusCircle
-                                size='6vw' 
-                                color='grey'
-                            />
+                        <div className='netflix-profile-card' onClick={badPathClick}>
+                            <img src={profile3} className='netflix-profile-img' alt='first profile'></img>
+                            <p>Kids</p>
                         </div>
-                        <p>Add Profile</p>
+
+                        <div className='netflix-profile-card' onClick={badPathClick}>
+                            <div className='netflix-profile-img'>
+                                <FaPlusCircle
+                                    size='6vw' 
+                                    color='grey'
+                                />
+                            </div>
+                            <p>Add Profile</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div 
-                id='netflix-manage-profiles-btn'
-                onClick={badPathClick}
-            >
-                Manage Profiles
+                <div 
+                    id='netflix-manage-profiles-btn'
+                    onClick={badPathClick}
+                >
+                    Manage Profiles
+                </div>
             </div>
         </div>
     )
