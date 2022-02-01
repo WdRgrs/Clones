689d8f2e88d7ab340365793cc66ccad80c5ce217
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-02-01 14:23:37 -0600

    a

--- src/netflix/NetflixVideoTitle.js
+++ src/netflix/NetflixVideoTitle.js
@@ -12,23 +12,35 @@ function NetflixVideoTitle({showInfo, setShowInfo, playingState}) {
         setShowInfo(true)
     }
 
-    let contentInfo = (
-                <p id='netflix-main-title-about'>
-                    Decades after the tournament that changed their lives, the rivalry between Johnny and Daniel reignites in this sequel to the "Karate Kid" films.
-                </p>
+    let titleSmall = (
+        <div>
+
+        <div id='netflix-title-series'>
+                    <SiNetflix color='red' size='1.2vw' />
+                    <span>{'SERIES'}</span>
+        </div>
+        <h2 id='deadstock-main-title'><span>C</span>obra <span>K</span>ai</h2>
+        </div>
+    )
+    let titleLarge = (
+        <div>
+
+        <div id='netflix-title-series-large'>
+                    <SiNetflix color='red' size='2.2vw' />
+                    <span>{'SERIES'}</span>
+        </div>
+        <h2 id='deadstock-main-title-large'><span>C</span>obra <span>K</span>ai</h2>
+        <p id='netflix-main-title-about'>
+            Decades after the tournament that changed their lives, the rivalry between Johnny and Daniel reignites in this sequel to the "Karate Kid" films.
+        </p>
+        </div>
     )
 
     return (
         <div>
             <div id='netflix-main-video-title'>
-                <div id='netflix-title-series'>
-                    <SiNetflix color='red' size='1.3vw' />
-                    <span>{'SERIES'}</span>
-                </div>
-                <h2 id='deadstock-main-title'><span>C</span>obra <span>K</span>ai</h2>
-
                 {/* UPDATE LOGIC TO HAVE SHOW INFORMATION ON FINISHED PLAYING */}
-                {playingState ? null : contentInfo}
+                {playingState ? titleSmall : titleLarge}
 
                 <section className='netflix-main-title-section' id='netflix-main-title-buttons-container'>
                     <button className='main-title-button' id='main-title-play'>
