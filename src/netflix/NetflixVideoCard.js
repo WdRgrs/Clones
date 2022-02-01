f48efd89fc68d76ef1c500f64d22646776ea379e
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2022-02-01 17:19:45 -0600

    a

--- src/netflix/NetflixVideoCard.js
+++ src/netflix/NetflixVideoCard.js
@@ -15,7 +15,6 @@ function NetflixVideoCard({showInfo, setShowInfo}) {
     return (
         <div id='netflix-card-background'>
             <div id='netflix-card'>
-    
 
                 <div className='video-background-card'>
                     <ReactPlayer
@@ -34,75 +33,78 @@ function NetflixVideoCard({showInfo, setShowInfo}) {
                         // onEnded={()=>handleDone()}
                     />
                 </div>
-                <div id='netflix-card-close-btn'
-                    onClick={()=>handleClose()}
-                >
-                    {'+'}
+
+                <div id='netflix-card-info'>
+                    <div id='netflix-card-close-btn'
+                        onClick={()=>handleClose()}
+                    >
+                        {'+'}
+                    </div>
+
+                    <h2 id='deadstock-title'><span>C</span>obra <span>K</span>ai</h2>
+                    <section className='netflix-title-section' id='netflix-title-info'>
+                        <div>New</div>
+                        <div>2020</div>
+                        <div id='netflix-rating'>TV-14</div>
+                        <div>4 Seasons</div>
+                        {/* <div id='netflix-audio-description'> */}
+                            <span className="iconify" data-icon="fa:audio-description" data-width="40"></span>
+                        {/* </div> */}
+                    </section>
+                    <p id='netflix-title-about'>
+                        Decades after the tournament that changed their lives, the rivalry between Johnny and Daniel reignites in this sequel to the "Karate Kid" films.
+                    </p>
+                    <section className='netflix-title-section' id='netflix-title-buttons'>
+                        <button>Play</button>
+                        <button>+ My List</button>
+                        <BsHandThumbsUp
+                            size='1.7em'
+                            style={{
+                                border: '1px solid white',
+                                borderRadius: '50%',
+                                padding: '.5em',
+                                margin: '0 .5em'
+                            }}
+                        />
+                        <BsHandThumbsDown 
+                            size='1.7em'
+                            style={{
+                                border: '1px solid white',
+                                borderRadius: '50%',
+                                padding: '.5em'
+                            }}
+                        />
+
+                    </section>
+                    <section className='netflix-title-section' id='netflix-title-details'>
+                        <div>
+                            <h3>Starring:</h3>
+                            <p>Ralph Macchio, William Zabka</p>
+                        </div>
+                        <div>
+                            <h3>Genres:</h3>
+                            <p>TV Dramas</p>
+                        </div>
+                        <div>
+                            <h3>This show is:</h3>
+                            <p>Heartfelt, Exciting</p>
+                        </div>
+                    </section>
+                    <section className='netflix-title-section' id='netflix-title-details'>
+                        <div>
+                            <h3>Starring:</h3>
+                            <p>Ralph Macchio, William Zabka</p>
+                        </div>
+                        <div>
+                            <h3>Genres:</h3>
+                            <p>TV Dramas</p>
+                        </div>
+                        <div>
+                            <h3>This show is:</h3>
+                            <p>Heartfelt, Exciting</p>
+                        </div>
+                    </section>
                 </div>
-
-                <h2 id='deadstock-title'><span>C</span>obra <span>K</span>ai</h2>
-                <section className='netflix-title-section' id='netflix-title-info'>
-                    <div>New</div>
-                    <div>2020</div>
-                    <div id='netflix-rating'>TV-14</div>
-                    <div>4 Seasons</div>
-                    {/* <div id='netflix-audio-description'> */}
-                        <span className="iconify" data-icon="fa:audio-description" data-width="40"></span>
-                    {/* </div> */}
-                </section>
-                <p id='netflix-title-about'>
-                    Decades after the tournament that changed their lives, the rivalry between Johnny and Daniel reignites in this sequel to the "Karate Kid" films.
-                </p>
-                <section className='netflix-title-section' id='netflix-title-buttons'>
-                    <button>Play</button>
-                    <button>+ My List</button>
-                    <BsHandThumbsUp
-                        size='1.7em'
-                        style={{
-                            border: '1px solid white',
-                            borderRadius: '50%',
-                            padding: '.5em',
-                            margin: '0 .5em'
-                        }}
-                    />
-                    <BsHandThumbsDown 
-                        size='1.7em'
-                        style={{
-                            border: '1px solid white',
-                            borderRadius: '50%',
-                            padding: '.5em'
-                        }}
-                    />
-
-                </section>
-                <section className='netflix-title-section' id='netflix-title-details'>
-                    <div>
-                        <h3>Starring:</h3>
-                        <p>Ralph Macchio, William Zabka</p>
-                    </div>
-                    <div>
-                        <h3>Genres:</h3>
-                        <p>TV Dramas</p>
-                    </div>
-                    <div>
-                        <h3>This show is:</h3>
-                        <p>Heartfelt, Exciting</p>
-                    </div>
-                </section>
-                <section className='netflix-title-section' id='netflix-title-details'>
-                    <div>
-                        <h3>Starring:</h3>
-                        <p>Ralph Macchio, William Zabka</p>
-                    </div>
-                    <div>
-                        <h3>Genres:</h3>
-                        <p>TV Dramas</p>
-                    </div>
-                    <div>
-                        <h3>This show is:</h3>
-                        <p>Heartfelt, Exciting</p>
-                    </div>
-                </section>
             </div>
         </div>
     )
